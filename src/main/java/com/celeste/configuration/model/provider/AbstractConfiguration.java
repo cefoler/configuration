be7bc73fd949f52d40404ed9adff968e95faa9ee
DeserRC <DeserRC@gmail.com>
2021-05-30 08:04:12 -0300

    Gradle version

--- src/main/java/com/celeste/configuration/model/provider/AbstractConfiguration.java
+++ src/main/java/com/celeste/configuration/model/provider/AbstractConfiguration.java
@@ -152,7 +152,7 @@ public abstract class AbstractConfiguration<U extends TokenStreamFactory> implem
         result.put(key, new LinkedHashMap<>());
       }
 
-      if (key.equals(lastPath) && result.containsKey(lastPath)) {
+      if (key.equals(lastPath)) {
         if (object == null) {
           result.remove(lastPath);
           return;
