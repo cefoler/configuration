9d3782bf4cb9502080776d4ea2e493bf5a6b9b3b
DeserRC <deserrc@gmail.com>
2022-04-01 03:09:04 -0300

    Added Scripts from setup jdk

--- script/setup-jdk.sh
+++ script/setup-jdk.sh
@@ -12,14 +12,29 @@ die() {
   exit 1
 }
 
-VERSION="$1"
-REGEX="[0-9]+"
-
-if [[ ! $VERSION =~ $REGEX ]]; then
-  die "Invalid JDK version: $VERSION"
+if [ -z "$1" ]; then
+  while [ -z "$request" ]; do
+    read -p "Enter the JDK version to install: " request
+    VERSION=$request
+  done
+else
+  VERSION=$1
 fi
 
+REGEX="[0-9]+"
+
+while [[ ! $VERSION =~ $REGEX ]]; do
+  read -p "Invalid JDK version, please enter a valid JDK version: " request
+  VERSION=$request
+done
+
 URL="https://corretto.aws/downloads/latest/amazon-corretto-$VERSION-x64-linux-jdk.tar.gz"
+CODE=$(curl --write-out %{http_code} --silent --output /dev/null "$URL")
+
+if [ "$CODE" -ne 200 ]; then
+  die "Failed to make the request to $URL"
+  exit 1
+fi
 
 FILE="amazon-corretto-$VERSION-x64-linux-jdk.tar.gz"
 DIRECTORY="/usr/lib/jvm/java-$VERSION-amazon-corretto"
